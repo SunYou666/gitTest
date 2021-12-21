b68d9cde7d62c2f0c9cb42194455923fe665dc87
sunyou <416343458@qq.com>
2021-12-21 22:47:39 +0800

    master first commit

--- src/main/java/com/you/git/GitTest.java
+++ src/main/java/com/you/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello, git!");
         System.out.println("hello, github!");
         System.out.println("hello, git4!");
+        System.out.println("master test");
     }
 
 }
