5fe4a119baaaa0002e27579a163ef0691ca6fa1e
sunyou <416343458@qq.com>
2021-12-21 20:16:35 +0800

    first commit

---
+++ src/main/java/com/you/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.you.git;
+
+/**
+ * @author systart
+ * @create 2021-12-21 20:13
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+
+}
